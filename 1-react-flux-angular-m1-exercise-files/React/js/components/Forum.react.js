d87ebe1674ad6e91e10f81a0aa20ecb9aa7c1699
David Dinh <dmdinh22@gmail.com>
2018-09-27 22:55:47 -0700

    pull ForumHeader component into main Forum component

--- 1-react-flux-angular-m1-exercise-files/React/js/components/Forum.react.js
+++ 1-react-flux-angular-m1-exercise-files/React/js/components/Forum.react.js
@@ -1,5 +1,9 @@
 const Forum = React.createClass({
     render: function() {
-        return React.createElement('div', null);
+        return React.createElement(
+            'div',
+            null,
+            React.createElement(ForumHeader, null)
+        );
     }
 });
