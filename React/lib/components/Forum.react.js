8d936dd931361d6474c079df513081fcc0171943
David Dinh <dmdinh22@gmail.com>
2018-09-29 01:51:29 -0700

    add components into main Forum component, add correct reference files and order

--- React/lib/components/Forum.react.js
+++ React/lib/components/Forum.react.js
@@ -24,7 +24,21 @@ var Forum = React.createClass({
         return React.createElement(
             'div',
             null,
-            React.createElement(ForumHeader, { allAnswers: this.state.allAnswers })
+            React.createElement(ForumHeader, null),
+            React.createElement(
+                'div',
+                { className: 'container' },
+                React.createElement(ForumQuestion, null),
+                React.createElement('hr', null),
+                React.createElement(ForumAnswers, { allAnswers: this.state.allAnswers }),
+                React.createElement('hr', null),
+                React.createElement(
+                    'h4',
+                    null,
+                    'Add an answer'
+                ),
+                React.createElement(ForumAddAnswerBox, null)
+            )
         );
     }
 });
