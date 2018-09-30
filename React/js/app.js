f2c4fd356e3158327e56f25a64c50ec864a6305a
David Dinh <dmdinh22@gmail.com>
2018-09-30 00:18:18 -0700

    implement EventEmitter

--- React/js/app.js
+++ React/js/app.js
@@ -2,3 +2,15 @@ React.render(
     React.createElement(Forum, null),
     document.getElementById('forum')
 );
+
+const myEmitter = new EventEmitter();
+
+myEmitter.on('STARTED_THE_APP', function() {
+    console.log('started the app');
+});
+
+myEmitter.on('STARTED_THE_APP', function() {
+    console.log('started the app #2');
+});
+
+myEmitter.EventEmitter('STARTED_THE_APP');
