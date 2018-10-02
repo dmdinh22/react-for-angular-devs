b518d554a2a0d3690e2b040bc5d81f90a1cc88e5
David Dinh <dmdinh22@gmail.com>
2018-10-02 00:16:47 -0700

    create metods to handle onChange and onClick

--- React/js/components/ForumAddAnswerBox.react.js
+++ React/js/components/ForumAddAnswerBox.react.js
@@ -1,10 +1,35 @@
 const ForumAddAnswerBox = React.createClass({
+    getInitialState: function() {
+        return {
+            value: ''
+        };
+    },
+
+    _addAnswer: function() {
+        this.props.onAddAnswer(this.state.value);
+    },
+
+    _onChange: function(event) {
+        this.setState({
+            value: event.target.value
+        });
+    },
+
     render: function() {
         return (
             <div>
-                <textarea className="col-md-6 col-xs-8" id="addAnswer" />
+                <textarea
+                    className="col-md-6 col-xs-8"
+                    id="addAnswer"
+                    onChange={this._onChange}
+                />
                 &nbsp;
-                <input type="button" className="btn btn-primary" value="Add" />
+                <input
+                    type="button"
+                    className="btn btn-primary"
+                    value="Add"
+                    onClick={this._addAnswer}
+                />
             </div>
         );
     }
